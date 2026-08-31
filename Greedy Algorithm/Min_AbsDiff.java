9a8f270f654a66e07b1ecbfe5065d82d296cadfb
Md Asif <asif212327@gmail.com>
2026-09-01 01:16:39 +0530

    Add files via upload

---
+++ Greedy Algorithm/Min_AbsDiff.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Min_AbsDiff {
+    public static void main(String args[]){
+        int A[]={1,2,3};
+        int B[]={2,1,3};
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int min=0;
+        for(int i=0;i<A.length;i++){
+            min=Math.abs(A[i]-B[i]);
+        }
+        System.out.println(min);
+    }
+}
